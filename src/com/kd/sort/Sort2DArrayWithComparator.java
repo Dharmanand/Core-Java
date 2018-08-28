ed70fe2e047d87f5d6c30c90d2eb21faaa51c3f5
Dharmanand <Dharmanand@users.noreply.github.com>
2018-08-28 08:45:09 +0530

    Sorting an array using 2D array as well as Arrays.sort method using Comparator logic that contains original array indexes as well

---
+++ src/com/kd/sort/Sort2DArrayWithComparator.java
@@ -0,0 +1,30 @@
+package com.kd.test.sort.array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Sort2DArrayWithComparator {
+
+	public static void main(String[] args) {
+		int nums [] = {5,2,7,8,4,11,15,10};
+ 		int n = nums.length;
+ 		
+ 		int [][] a = new int [n][2];
+ 		for(int i=0; i<n; i++){
+ 			a[i][0] = nums[i];
+ 			a[i][1] = i;
+ 		}
+
+ 		Arrays.sort(a, new Comparator<int[]>(){
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(o1[0], o2[0]);
+			}
+ 		});
+ 		
+ 		for(int i=0; i<n; i++){
+ 			System.out.println("value: "+a[i][0]);
+ 			System.out.println("Original Index: "+a[i][1]);
+ 		}
+	}
+}
