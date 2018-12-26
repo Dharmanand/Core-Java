d5a041a965dd0ba00c9f057af36df1a3f0357171
Dharmanand <kumar.kd20@gmail.com>
2018-12-26 07:14:17 +0530

    comment added

--- src/com/kd/threads/callableAndFuture/CallableAndFutureApp.java
+++ src/com/kd/threads/callableAndFuture/CallableAndFutureApp.java
@@ -7,6 +7,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/*
+ * Use of Future and Callable with thread.
+ * 
+ */
+
 public class CallableAndFutureApp {
 
 	public static void main(String[] args) {
