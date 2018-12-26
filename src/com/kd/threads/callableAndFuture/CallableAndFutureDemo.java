5de564e9d2f8701ffd7a8199d45a58eaff8acb2c
Dharmanand <kumar.kd20@gmail.com>
2018-12-26 07:36:00 +0530

    Added comment

--- src/com/kd/threads/callableAndFuture/CallableAndFutureDemo.java
+++ src/com/kd/threads/callableAndFuture/CallableAndFutureDemo.java
@@ -6,7 +6,22 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
+/*
+ * Future: A Future represents the result of an asynchronous computation. 
+ * 		   Methods are provided to check if the computation is complete, 
+ * 		   to wait for its completion, and to retrieve the result of the computation. 
+ * 		   The result can only be retrieved using method get when the computation has 
+ * 		   completed, blocking if necessary until it is ready. 
+ * 		   Cancellation is performed by the cancel method.
+ * 
+ * Callable: A task that returns a result and may throw an exception. 
+ * 			 Implementors define a single method with no arguments called call. 
+ * 			 The Callable interface is similar to java.lang.Runnable, in that both are 
+ * 			 designed for classes whose instances are potentially executed by another 
+ * 			 thread. A Runnable, however, does not return a result and cannot throw a 
+ * 			 checked exception. 
+ * 
+ */
 public class CallableAndFutureDemo {
 
 	public static void main(String[] args) {
