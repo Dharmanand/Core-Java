76a06044f2ed3213235c3b00d08cb8b8d1396e86
Dharmanand <kumar.kd20@gmail.com>
2019-01-05 04:32:21 +0530

    CTC - The blank final field s may not have been initialized

---
+++ src/com/kd/basic/interf/InterfTest1.java
@@ -0,0 +1,9 @@
+package com.kd.basic.interf;
+
+public interface InterfTest1 {
+	
+	public static final int i=10;
+	final float f = 100.0f;
+	static public String s;//CTC - The blank final field s may not have been initialized
+	
+}
