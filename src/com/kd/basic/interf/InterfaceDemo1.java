40435d06cdc3623e3bd6adaf667a89a1e3fb3635
Dharmanand <kumar.kd20@gmail.com>
2019-01-05 04:10:18 +0530

    added output comment

--- src/com/kd/basic/interf/InterfaceDemo1.java
+++ src/com/kd/basic/interf/InterfaceDemo1.java
@@ -5,7 +5,7 @@ public class InterfaceDemo1 implements C{
 	int i=3;
 	public static void main(String[] args) {
 		C c1 = new InterfaceDemo1();
-		System.out.println(c1.i);
+		System.out.println(c1.i);//output: 2
 	}
 
 }
