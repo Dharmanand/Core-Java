bd04a712076e79f17a140e79091bc64e44a461cd
Dharmanand <kumar.kd20@gmail.com>
2019-01-05 04:09:35 +0530

    instance variable wrt Inheritance

---
+++ src/com/kd/basic/interf/InterfaceDemo1.java
@@ -0,0 +1,23 @@
+package com.kd.basic.interf;
+
+public class InterfaceDemo1 implements C{
+
+	int i=3;
+	public static void main(String[] args) {
+		C c1 = new InterfaceDemo1();
+		System.out.println(c1.i);
+	}
+
+}
+
+interface A {
+	int i=0;
+}
+
+interface B extends A {
+	int i=1;
+}
+
+interface C extends B {
+	int i=2;
+}
