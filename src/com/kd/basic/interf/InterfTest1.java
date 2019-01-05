6529357451a80490eb702c2a7ca237fec08f5267
Dharmanand <kumar.kd20@gmail.com>
2019-01-05 05:54:00 +0530

    comment updated

--- src/com/kd/basic/interf/InterfTest1.java
+++ src/com/kd/basic/interf/InterfTest1.java
@@ -4,6 +4,6 @@ public interface InterfTest1 {
 	
 	public static final int i=10;
 	final float f = 100.0f;
-	static public String s;//CTC - The blank final field s may not have been initialized
+	static public String s;//CTE - The blank final field s may not have been initialized
 	
 }
