b0327beaa99f7d0fedcd551cc2c1efad8f036252
Dharmanand <kumar.kd20@gmail.com>
2019-01-25 09:14:27 +0530

    CE : The method add(Integer, Integer) is ambiguous for the type OverloadedMethodNullCheck

---
+++ src/oops/OverloadedMethodNullCheck.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class OverloadedMethodNullCheck {
+
+	public Integer add(Integer i, Integer j) {
+		return i + j;
+	}
+	public Float add(Float i, Float j) {
+		return i + j;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(add(null, null));//CTE : The method add(Integer, Integer) is ambiguous for the type OverloadedMethodNullCheck
+	}
+
+}
