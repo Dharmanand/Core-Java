4146ac779b7c173ce2a3b7285242a7f7008f4be3
ab55676 <ab55676@SGH638RJXP.ctl.intranet>
2019-02-18 15:42:42 +0530

    Unsupported Operation Exception

--- src/com/kd/trickeyQA/ArraysAsList.java
+++ src/com/kd/trickeyQA/ArraysAsList.java
@@ -17,13 +17,14 @@ public class ArraysAsList {
 
 	public static void main(String[] args) {
 
+		// -------------------with int-----------------
 		int[] a = { 1, 2, 8, 6 };
 		int[] a2 = { 1, 2, 8, 6 };
 
 		List<int[]> list = Arrays.asList(a);// Look here List<int[]> has int[] Type
 		list.add(a2);// (Runtime)Exception in thread "main" java.lang.UnsupportedOperationException
 		System.out.println(list);
-		// ------------------------------------
+		// -------------------with Integer-----------------
 		Integer b[] = { 1, 5, 9, 8, 6 };
 		List<Integer> list1 = Arrays.asList(b);// But here List<Integer> has Integer Type
 		list1.add(2);// (Runtime)Exception in thread "main" java.lang.UnsupportedOperationException
