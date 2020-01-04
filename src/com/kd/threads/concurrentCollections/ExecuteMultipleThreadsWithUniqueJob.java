fa359369d822d63eb591379492c78d7bb97ca910
kumar <kumar@DESKTOP-9EJSM9D>
2020-01-04 07:10:45 +0530

    Added code to track and capture all unique threads during jab executions.

--- src/com/kd/threads/concurrentCollections/ExecuteMultipleThreadsWithUniqueJob.java
+++ src/com/kd/threads/concurrentCollections/ExecuteMultipleThreadsWithUniqueJob.java
@@ -12,18 +12,22 @@ public class ExecuteMultipleThreadsWithUniqueJob {
 	public static void main(String[] args) throws InterruptedException {
 
 //		CopyOnWriteArraySet<Integer> set = new CopyOnWriteArraySet<>();//Performace bies it is not good
-//		ConcurrentSkipListSet<Integer> set = new ConcurrentSkipListSet<>();//It is faster than CopyOnWriteArraySet, but in search/Iteration It will take little time.
+//		ConcurrentSkipListSet<String> set = new ConcurrentSkipListSet<>();//It is faster than CopyOnWriteArraySet, but in search/Iteration It will take little time.
 		
 		Set<Integer> set = new ConcurrentHashMap<Integer, Integer>().newKeySet();//Herecreated cocurrentHashSet using cocurrentHashMap and newKeySet() method. It 	is faster than CopyOnWriteArraySet and ConcurrentSkipListSet.
+		Set<String> set1 = new ConcurrentHashMap<String, Integer>().newKeySet();//Maintain a group of all unique threads during jab executions. 
 
 		ExecutorService service = Executors.newFixedThreadPool(10000);
 		for (int i = 0; i < 10000; i++) {
+			set1.add(Thread.currentThread().getName());
 			service.execute(() -> {
+				set1.add(Thread.currentThread().getName());
 				for (int j = 0; j < 10000; j++) {
 					set.add(j);
 				}
 			});
 			service.execute(() -> {
+				set1.add(Thread.currentThread().getName());
 				for (int j = 100; j < 1200; j++) {
 					if (set.contains(j))
 						set.remove(j);
@@ -32,9 +36,9 @@ public class ExecuteMultipleThreadsWithUniqueJob {
 		}
 		service.shutdown();
 		service.awaitTermination(1, TimeUnit.MINUTES);
-		System.out.println(set.size());
-		Thread.sleep(10000);
-		System.out.println(set.size());
+		System.out.println("Toal data : "+set.size());
+		System.out.println("Toal Threads : "+set1.size());
+		set1.stream().forEach(i -> System.out.println(i));
 
 	}
 
